c52fb4b0f84f3b0cdccc2b2bc84f20d43bec5aa7
alexanderNB <alexander@bjerking.dk>
2024-10-10 11:26:49 +0200

    Program der finder s og t.

---
+++ FindingST.py
@@ -0,0 +1,35 @@
+import math
+while(True):
+    # a is congruent to 1 mod m    a \equiv 1 \text{ mod } m
+    try:
+        a = int(input("Select a: "))        
+        m = int(input("Select m: "))
+    except:
+        continue
+    AnswerFound = False
+
+
+    mIsNegative = m<0
+
+    for i in range(abs(m)-1):
+        if mIsNegative:
+            if a*i%m != 1+m:
+                continue
+        elif a*i%m != 1:
+            continue
+
+        s = i
+        t = -math.floor((a*i)/m)
+        if mIsNegative:
+            t-=1
+
+        print(f"s = {s}")
+        print(f"t = {t}")
+        print()
+        print("sa+tm=1")
+        print(f"({s}*{a})+({t}*{m})=1")
+        AnswerFound = True
+    if not AnswerFound:
+        print("No answer")
+    print()
+    
