5fa4d01d5978fe8c53bd6acf8f8fcc6f2f33ede9
alexanderNB <alexander@bjerking.dk>
2024-11-05 15:31:51 +0100

    TrappeMatrix fixet

--- TrappeMatrix.py
+++ TrappeMatrix.py
@@ -108,6 +108,8 @@ def ref(A, total_matrix_shape = (0, 0), first_run = True, set_matrix = np.array(
         b = B[row, first_nonzero_position["column"]]
         if b == 0:
             continue
+        if b.denominator != 1 and non_zero_b_found:
+            step += "\\\\ \n\\vspace{-1.1em}"
         non_zero_b_found = True
         
         B[row] = B[row] - B[0] * b
@@ -121,6 +123,8 @@ def ref(A, total_matrix_shape = (0, 0), first_run = True, set_matrix = np.array(
             step += f"\\\\ \nR_{{{row + total_matrix_shape[0] - A_height + 1}}}\\leftarrow R_{{{row + total_matrix_shape[0] - A_height + 1}}} {fortegn} R_{{{total_matrix_shape[0]-A_height+1}}}"
         else:
             step += f"\\\\ \nR_{{{row + total_matrix_shape[0] - A_height + 1}}}\\leftarrow R_{{{row + total_matrix_shape[0] - A_height + 1}}} {fortegn} {b.abs().return_latex_str()}R_{{{total_matrix_shape[0]-A_height+1}}}"
+            if b.denominator != 1:
+                step += "\\\\ \n\\vspace{-1.1em}"
 
     if non_zero_b_found:
         step += "\n\\end{array}"
@@ -133,7 +137,7 @@ def ref(A, total_matrix_shape = (0, 0), first_run = True, set_matrix = np.array(
     set_matrix = try_concatenate(set_matrix, np.array([first_row]))
 
     C = B[1:]
-    C = ref(C, total_matrix_shape=total_matrix_shape, first_run=False, set_matrix=set_matrix, TeX=True) 
+    C = ref(C, total_matrix_shape=total_matrix_shape, first_run=False, set_matrix=set_matrix, TeX=True, result_matrix_line=result_matrix_line) 
     
     return np.vstack([first_row, C]) 
 
@@ -144,6 +148,7 @@ def reduced_ref(A, TeX = False, result_matrix_line = False):
     print()
     A_height, A_width = A.shape
 
+    pivot_count = 0
     for column_index in range(A_width):
         one_found = False
         old_A = A.copy()
@@ -154,15 +159,20 @@ def reduced_ref(A, TeX = False, result_matrix_line = False):
         column_changed = False
         for row_index in range(A_height):
             reversed_row_index = A_height-row_index-1
+            if reversed_row_index < pivot_count and not one_found:
+                continue
             if A[reversed_row_index, column_index] == 1 and not one_found:
                 one_found = True
                 one_index = reversed_row_index
+                pivot_count += 1
                 continue
             if not one_found:
                 continue
             current_to_be_removed = A[reversed_row_index, column_index]
             
             if current_to_be_removed != 0:
+                if current_to_be_removed.denominator != 1 and column_changed:
+                    step += "\\\\ \n\\vspace{-1.1em}"
                 column_changed = True
                 A[reversed_row_index] -= A[one_index]*current_to_be_removed
 
@@ -173,6 +183,8 @@ def reduced_ref(A, TeX = False, result_matrix_line = False):
                     step += f"\\\\ \nR_{{{reversed_row_index + 1}}} \\leftarrow R_{{{reversed_row_index + 1}}} {fortegn}R_{{{one_index + 1}}}"
                 else:
                     step += f"\\\\ \nR_{{{reversed_row_index + 1}}} \\leftarrow R_{{{reversed_row_index + 1}}} {fortegn}{current_to_be_removed.abs().return_latex_str()}R_{{{one_index + 1}}}"
+                    if current_to_be_removed.denominator != 1:
+                        step += "\\\\ \n\\vspace{-1.1em}"
         if column_changed:
             step += "\n\\end{array}"
             step += "\n" + PL.printMatrix(A, True, result_matrix_line)
@@ -181,6 +193,7 @@ def reduced_ref(A, TeX = False, result_matrix_line = False):
             else:
                 print(f"\\[\n{step}\n\\]")
     print("Over er den på reduceret trappeform")
+    print(f"Der er {pivot_count} pivot elementer")
     print()
     return A
 
@@ -193,15 +206,15 @@ def try_concatenate(matrix1, matrix2):
 
 A = np.array(
     [
-        [3, -1, 1],
-        [2, 2, 2],
-        [1, 5, 3],
-        [1, -3, -1]
+        [2,2,0],
+        [-1,1,2],
+        [0,2,2],
+        [0,1,1],
     ])
 
 
 print("Copy")
 print()
-reduced_ref(A, TeX=True, result_matrix_line=True)
+reduced_ref(A)
 print()
 print("End copy")
